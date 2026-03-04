bfafc6e42dca8f35691192bc8ed791e002eddc6b
Anderson <anderson@Andersons-MacBook-Air.local>
2026-03-04 15:07:29 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-const name = "";
+// ESM
+import Fastify from "fastify";
+import "dotenv/config";
 
-console.log(name);
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.PORT) });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
